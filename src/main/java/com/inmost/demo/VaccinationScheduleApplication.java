557eda969553982c1faf6d0945a676c9d4e91b59
Yurii Sienin <senin.yura99@gmail.com>
2021-02-22 18:18:04 +0200

    Initial commit

---
+++ src/main/java/com/inmost/demo/VaccinationScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.inmost.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inmost/demo/VaccinationScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmost.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
